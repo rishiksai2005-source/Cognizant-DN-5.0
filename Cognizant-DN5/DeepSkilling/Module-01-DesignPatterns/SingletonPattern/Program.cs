6e0330aa52ac3723d774aa09b659bce839de9b4a
Rishi <rishiksai2005@gmail.com>
2026-06-29 11:40:38 +0530

    Completed Exercise 1 - Singleton Pattern

---
+++ Cognizant-DN5/DeepSkilling/Module-01-DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the Singleton instance
+            Singleton obj1 = Singleton.GetInstance();
+            Singleton obj2 = Singleton.GetInstance();
+
+            obj1.DisplayMessage();
+            obj2.DisplayMessage();
+
+            Console.WriteLine();
+
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("Both objects are the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Objects are different.");
+            }
+        }
+    }
+}
