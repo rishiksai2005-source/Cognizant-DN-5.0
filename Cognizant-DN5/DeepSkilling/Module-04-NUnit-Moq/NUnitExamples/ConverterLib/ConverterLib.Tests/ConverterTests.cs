d2d440149434219e7f9d5b57cb68a66f23ed3e3c
Rishi <rishiksai2005@gmail.com>
2026-07-07 00:54:10 +0530

    Completed Module-04 NUnit and Moq Hands-ons

---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/ConverterLib/ConverterLib.Tests/ConverterTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using NUnit.Framework;
+using ConverterLib;
+
+namespace ConverterLib.Tests
+{
+    [TestFixture]
+    public class ConverterTests
+    {
+        private Mock<IDollarToEuroExchangeRateFeed> mockExchangeRate;
+        private Converter converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockExchangeRate = new Mock<IDollarToEuroExchangeRateFeed>();
+
+            // Mock exchange rate: 1 USD = 0.9 Euro
+            mockExchangeRate.Setup(x => x.GetActualUSDollarValue())
+                            .Returns(0.9);
+
+            converter = new Converter(mockExchangeRate.Object);
+        }
+
+        [Test]
+        public void USDToEuro_100USD_Returns90Euro()
+        {
+            double actual = converter.USDToEuro(100);
+
+            Assert.That(actual, Is.EqualTo(90));
+        }
+
+        [Test]
+        public void USDToEuro_50USD_Returns45Euro()
+        {
+            double actual = converter.USDToEuro(50);
+
+            Assert.That(actual, Is.EqualTo(45));
+        }
+
+        [Test]
+        public void USDToEuro_0USD_Returns0Euro()
+        {
+            double actual = converter.USDToEuro(0);
+
+            Assert.That(actual, Is.EqualTo(0));
+        }
+    }
+}
---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/ConverterLib/README.md
@@ -0,0 +1,27 @@
+# NUnit Handson 9 - ConverterLib (Moq)
+
+## Objective
+Learn how to write unit tests using the Moq framework by mocking external dependencies.
+
+## Project
+ConverterLib
+
+## Features Tested
+- USD to Euro conversion
+- Mocking exchange rate service
+- Dependency Injection
+- Isolated unit testing
+
+## NUnit Concepts Covered
+- TestFixture
+- SetUp
+- Assert.That
+- Mock Objects
+- Dependency Injection
+- Moq Framework
+
+## Result
+All unit tests executed successfully.
+
+## Outcome
+Successfully used Moq to mock the exchange rate dependency and verified the conversion logic independently.
