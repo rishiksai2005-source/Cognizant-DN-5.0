13d1295160d9aff73a8c9ca2fedca13bc4a20a4c
Rishi <rishiksai2005@gmail.com>
2026-07-06 23:42:36 +0530

    Completed NUnit Handson 7 - LeapYearCalculatorLib

---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/LeapYearCalculatorLib/LeapYearCalculatorLib.Tests/LeapYearCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using LeapYearCalculatorLib;
+
+namespace LeapYearCalculatorLib.Tests
+{
+    [TestFixture]
+    public class LeapYearCalculatorTests
+    {
+        private LeapYearCalculator leapYearCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            leapYearCalculator = new LeapYearCalculator();
+        }
+
+        // Leap Years
+        [TestCase(2000, 1)]
+        [TestCase(2024, 1)]
+        [TestCase(1996, 1)]
+        public void IsLeapYear_LeapYear_ReturnsOne(int year, int expected)
+        {
+            int actual = leapYearCalculator.IsLeapYear(year);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // Non-Leap Years
+        [TestCase(2023, 0)]
+        [TestCase(1900, 0)]
+        [TestCase(1999, 0)]
+        public void IsLeapYear_NonLeapYear_ReturnsZero(int year, int expected)
+        {
+            int actual = leapYearCalculator.IsLeapYear(year);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // Invalid Years
+        [TestCase(1700, -1)]
+        [TestCase(10000, -1)]
+        [TestCase(0, -1)]
+        public void IsLeapYear_InvalidYear_ReturnsMinusOne(int year, int expected)
+        {
+            int actual = leapYearCalculator.IsLeapYear(year);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/LeapYearCalculatorLib/README.md
@@ -0,0 +1,31 @@
+# NUnit Handson 7 - LeapYearCalculatorLib
+
+## Objective
+
+To implement parameterized NUnit tests for validating leap year calculations.
+
+## Project
+
+LeapYearCalculatorLib
+
+## Features Tested
+
+- Leap year validation
+- Non-leap year validation
+- Invalid year validation
+
+## NUnit Concepts Covered
+
+- TestFixture
+- SetUp
+- TestCase
+- Assert.That
+- Parameterized Tests
+
+## Verification
+
+Executed all unit tests successfully.
+
+## Outcome
+
+Successfully validated leap year logic using parameterized NUnit test cases and Assert.That().
