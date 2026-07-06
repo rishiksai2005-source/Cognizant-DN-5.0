760eca27cbe7ab0a66e11c1059194173c9093710
Rishi <rishiksai2005@gmail.com>
2026-07-06 20:36:33 +0530

    Completed Module-04 NUnit Handson 1 - Calculator Testing

---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/CalcLibrary/CalcLibrary/CalcLibrary.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        private SimpleCalculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(100, 200, 300)]
+        [TestCase(-5, 5, 0)]
+        public void Addition_ShouldReturnCorrectSum(double a, double b, double expected)
+        {
+            double result = calculator.Addition(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/CalcLibrary/README.md
@@ -0,0 +1,30 @@
+# NUnit Handson 1 - Calculator Testing
+
+## Objective
+
+Learn the basics of NUnit by writing unit tests for a simple calculator library.
+
+## Project
+
+CalcLibrary
+
+## Features Tested
+
+- Addition
+- Parameterized Tests
+
+## NUnit Concepts Covered
+
+- TestFixture
+- SetUp
+- Test
+- TestCase
+- Assert.That
+
+## Verification
+
+Executed multiple addition test cases using parameterized tests. All test cases passed successfully.
+
+## Outcome
+
+Successfully created and executed NUnit unit tests using TestFixture, SetUp, TestCase, and Assert.That.
