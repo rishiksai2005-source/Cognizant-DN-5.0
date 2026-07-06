21c401e48ad8fea5871e6b63a8ef81f899d8c0ac
Rishi <rishiksai2005@gmail.com>
2026-07-06 23:53:23 +0530

    Completed NUnit Handson 8 - UserManagerLib

---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/UserManagerLib/README.md
@@ -0,0 +1,33 @@
+# NUnit Handson 8 - UserManagerLib
+
+## Objective
+
+To test exception handling using NUnit.
+
+## Project
+
+UserManagerLib
+
+## Features Tested
+
+- Valid PAN card creation
+- Null PAN validation
+- Empty PAN validation
+- Invalid PAN length validation
+
+## NUnit Concepts Covered
+
+- TestFixture
+- SetUp
+- TestCase
+- Assert.That
+- Throws.TypeOf
+- Exception Testing
+
+## Verification
+
+Executed all unit tests successfully.
+
+## Outcome
+
+Successfully tested normal execution and exception scenarios using NUnit exception assertions.
---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/UserManagerLib/UserManagerLib.Tests/UserTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using UserManagerLib;
+using System;
+
+namespace UserManagerLib.Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        private User user;
+
+        [SetUp]
+        public void Setup()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void CreateUser_ValidPAN_DoesNotThrowException()
+        {
+            var newUser = new User
+            {
+                PANCardNo = "ABCDE1234F"
+            };
+
+            Assert.That(() => user.CreateUser(newUser), Throws.Nothing);
+        }
+
+        [Test]
+        public void CreateUser_NullPAN_ThrowsNullReferenceException()
+        {
+            var newUser = new User
+            {
+                PANCardNo = null
+            };
+
+            Assert.That(() => user.CreateUser(newUser),
+                Throws.TypeOf<NullReferenceException>());
+        }
+
+        [Test]
+        public void CreateUser_EmptyPAN_ThrowsNullReferenceException()
+        {
+            var newUser = new User
+            {
+                PANCardNo = ""
+            };
+
+            Assert.That(() => user.CreateUser(newUser),
+                Throws.TypeOf<NullReferenceException>());
+        }
+
+        [TestCase("ABC")]
+        [TestCase("ABCDE123456")]
+        [TestCase("12345")]
+        public void CreateUser_InvalidPANLength_ThrowsFormatException(string pan)
+        {
+            var newUser = new User
+            {
+                PANCardNo = pan
+            };
+
+            Assert.That(() => user.CreateUser(newUser),
+                Throws.TypeOf<FormatException>());
+        }
+    }
+}
