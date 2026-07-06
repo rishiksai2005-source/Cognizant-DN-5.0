a2bebe752013a1b43460517283df686d02a2be45
Rishi <rishiksai2005@gmail.com>
2026-07-06 23:12:09 +0530

    Completed NUnit Handson 5 - CollectionsLib

---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/CollectionsLib/CollectionsLib.Tests/EmployeeManagerTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using CollectionsLib;
+using System.Linq;
+
+namespace CollectionsLib.Tests
+{
+    [TestFixture]
+    public class EmployeeManagerTests
+    {
+        private EmployeeManager employeeManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            employeeManager = new EmployeeManager();
+        }
+
+        // Scenario 1
+        [Test]
+        public void GetEmployees_ShouldNotContainNull()
+        {
+            var employees = employeeManager.GetEmployees();
+
+            CollectionAssert.AllItemsAreNotNull(employees);
+        }
+
+        // Scenario 2
+        [Test]
+        public void GetEmployees_ShouldContainFourEmployees()
+        {
+            var employees = employeeManager.GetEmployees();
+
+            ClassicAssert.AreEqual(4, employees.Count);
+        }
+
+        // Scenario 3
+        [Test]
+        public void GetEmployees_ShouldContainUniqueEmployees()
+        {
+            var employees = employeeManager.GetEmployees();
+
+            CollectionAssert.AllItemsAreUnique(employees);
+        }
+
+        // Scenario 4
+        [Test]
+        public void GetEmployees_ShouldContainJohn()
+        {
+            var employees = employeeManager.GetEmployees();
+
+            CollectionAssert.Contains(
+                employees.Select(e => e.EmpName),
+                "John");
+        }
+
+        // Scenario 5
+        [Test]
+        public void GetEmployeesWhoJoinedInPreviousYears_ShouldReturnEmployees()
+        {
+            var employees = employeeManager.GetEmployeesWhoJoinedInPreviousYears();
+
+            CollectionAssert.AllItemsAreNotNull(employees);
+            ClassicAssert.AreEqual(4, employees.Count);
+        }
+    }
+}
---
+++ Cognizant-DN5/DeepSkilling/Module-04-NUnit-Moq/NUnitExamples/CollectionsLib/README.md
@@ -0,0 +1,32 @@
+# NUnit Handson 5 - CollectionsLib
+
+## Objective
+
+Learn and implement NUnit CollectionAssert methods to validate collections.
+
+## Project
+
+CollectionsLib
+
+## Features Tested
+
+- Collection contains no null values
+- Employee collection count validation
+- Unique employee objects
+- Collection contains expected employee
+- Employees joined in previous years
+
+## NUnit Concepts Covered
+
+- TestFixture
+- SetUp
+- CollectionAssert
+- ClassicAssert
+
+## Verification
+
+Executed all unit tests successfully.
+
+## Outcome
+
+Successfully validated employee collections using NUnit CollectionAssert methods.
